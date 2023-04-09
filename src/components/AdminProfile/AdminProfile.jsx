7067106bd6dd5b55b05fa666e63ac664f1187f8c
RicoJap <ricojap@gmail.com>
2023-04-10 00:25:59 +0700

    add signout button and alert

--- src/components/AdminProfile/AdminProfile.jsx
+++ src/components/AdminProfile/AdminProfile.jsx
@@ -2,23 +2,27 @@ import React, { useState } from 'react';
 import { Col, Container, Row, Form, Dropdown, Button } from 'react-bootstrap';
 import { NAPOLEON_BROWN_COLOR } from '../../utils/colors';
 import { MEMBERSHIP_PLANS, SIX_MONTHS_IN_MILLISECONDS, THREE_MONTHS_IN_MILLISECONDS } from '../../utils/constants';
-import Webcam from "react-webcam";
 import { AiFillCamera } from 'react-icons/ai'
 import CameraModal from '../CameraModal/CameraModal';
 import firebase from '../../utils/firebase';
 import { ref, set } from "firebase/database";
 import { sanitizeEmail } from '../../utils/utils';
 import { useNavigate } from 'react-router-dom';
+import Alert from 'react-bootstrap/Alert';
+import { signOut } from "firebase/auth";
+import useLogin from '../../hooks/useLogin';
 
 const AdminProfile = () => {
 
-  const { database } = firebase();
+  const { auth, database } = firebase();
   const navigate = useNavigate();
+  const { setUser } = useLogin();
 
   const [selectedMembershipOption, setSelectedMembershipOption] = useState('3');
   const [pictureMode, setPictureMode] = useState(false);
   const [email, setEmail] = useState(null);
   const [memberPicture, setMemberPicture] = useState(null);
+  const [alertVariant, setAlertVariant] = useState(null);
 
   const renderDropdownToggleText = () => {
     if(selectedMembershipOption === '3') {
@@ -55,12 +59,39 @@ const AdminProfile = () => {
 
       try {
         await set(ref(database, `users/${sanitizedEmail}`), payload)
-        // TODO: Add a success alert
-        navigate('/');
+        setAlertVariant('success');
+        // navigate('/');
       } catch(err) {
         console.error(err);
+        setAlertVariant('danger')
       }
     }
+  }
+
+  const renderAlert = () => {
+    if(alertVariant === 'success') {
+      return (
+        <Alert variant="success" style={{width: '100%'}}>
+          New member created
+        </Alert>
+      )
+    } else if(alertVariant === 'danger') {
+      return (
+        <Alert variant="danger" style={{width: '100%'}}>
+          Something is wrong when creating a new member
+        </Alert>
+      )
+    }
+  }
+
+  const onSignoutClick = () => {
+    setUser(null);
+    signOut(auth).then(() => {
+      localStorage.removeItem('user');
+      navigate('/')
+    }).catch(() => {
+      console.error('Trouble signing out')
+    })
   }
 
   return (
@@ -68,6 +99,7 @@ const AdminProfile = () => {
       <Row>
         <Col>
           <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
+            {renderAlert()}
             <h3 style={{paddingBottom: 50}}>Enter new membership plan</h3>
             <Form style={{width: '75%'}} onSubmit={onSubmitMembershipPlan}>
               <Form.Group style={{paddingBottom: 20}}>
@@ -89,7 +121,7 @@ const AdminProfile = () => {
               
               {pictureMode && <CameraModal memberPicture={memberPicture} setMemberPicture={setMemberPicture} show={pictureMode} handleClose={handleCloseCameraModal} />}
 
-              <Dropdown style={{paddingBottom: 50, width: '100%'}} onSelect={(evtKey) => setSelectedMembershipOption(evtKey)}>
+              <Dropdown style={{paddingBottom: 100, width: '100%'}} onSelect={(evtKey) => setSelectedMembershipOption(evtKey)}>
                 <Dropdown.Toggle style={{width: '100%', backgroundColor: NAPOLEON_BROWN_COLOR, borderColor: NAPOLEON_BROWN_COLOR}}>
                   {renderDropdownToggleText()}
                 </Dropdown.Toggle>
@@ -102,9 +134,10 @@ const AdminProfile = () => {
                   })}
                 </Dropdown.Menu>
               </Dropdown>
-              <Button style={{width: '100%'}} variant="success" type="submit">
+              <Button style={{width: '100%', marginBottom: 10}} variant="success" type="submit">
                 Submit
               </Button>
+              <Button style={{width: '100%'}} variant="danger" onClick={onSignoutClick}>Sign out</Button>
             </Form>
           </div>
         </Col>
